5d60d3bdb7dbce291de17912cdd92b152709aa1b
sheikh-mamun <abalmamun2515@gmail.com>
2024-01-26 01:47:14 +0600

    up

---
+++ array/demo.js
@@ -0,0 +1,12 @@
+/*console.log(4+3 + "<\n");
+document.write( "total" + " " +5+5 + "<br>");
+document.write( "total" + " " + 5+5 + "<br>");
+
+
+var salt = 20;
+
+var price = 40;
+var age =30;
+document.write("salt", "price" ,"age");*/
+
+var hero = typeof 'Hero Alom-22';
---
+++ java-1/sortmethod.js
@@ -0,0 +1,115 @@
+/*var a =['rafia', 'foisal', 'karim'];
+a.push('jamal');
+a.push('junayed');
+console.log(a);
+
+
+
+var a =['rafia', 'foisal', 'karim', 'abdullah', 'balam'];
+a.sort();
+console.log(a);
+*/
+
+// object
+
+// var yamaha ={
+//   price: 200,
+//   color: 'black',
+//   size: 'large',
+//   brand: 'japani',
+//   feature: 2023,
+//   place:['dhaka', 'barisal', 'komilla', 'rajshahi', 'sirajgonj', 'rangpur']
+// };
+
+// console.log(yamaha['place']);
+
+// new object
+
+// var yamaha ={price: 200, color:'black', brand:"japani", feature:2023};
+
+// var yamaha ={
+//   price: 200,
+//   color:'black',
+//   brand:"japani",
+//   feature:2023,
+//   size:'big',
+//   beautiful:false,
+//   design:['small', 'big', 'midium','large'],
+//   nextobj:{
+//     color:'blue',
+//     size:'small',
+//   }
+    
+// };
+
+// Object.freeze(yamaha);
+// delete yamaha.size;
+
+
+// console.log('brand');
+
+// const name = 'raisha';
+// const nam ='karim';
+// name. ('foisal');
+
+// console.log(nam,name);
+
+// const price =20;
+// const name ='raju';
+
+// console.log(price);
+
+// Mathmatical term
+
+// const eggPrice = 60;
+// const saltPrice = 40;
+// const total = eggPrice + saltPrice;
+
+// console.log(eggPrice + saltPrice);
+
+// const income = 4000;
+// const cost = 1000;
+// result = income / cost;
+// console.log(result);
+
+// let firstName = 'karim';
+// let lastName = "jaman";
+// let profession = 'teacher';
+
+// console.log( "biography:" 'firstName' + " " + lastName + " " + profession);
+
+
+// var num1 = 'karim';
+// var num2 ='foisal';
+// console.log('num1');
+
+// const num1 = 'jahir';
+// const num2 = 'karim';
+
+
+// let burger = 500;
+// let budjet = 200;
+// if(burger < budjet){
+// console.log('burger khabo');
+// }
+// else{
+//   console.log('khabo na');
+// }
+
+// funstion
+
+function summ (a,b){
+  document.write(a+b);
+}
+summ(3,7);
+
+
+
+
+
+
+
+
+
+
+
