cd3a6540feccc25c52ff8f082079b332662cbc44
sheikh-mamun <abalmamun2515@gmail.com>
2024-02-07 12:54:29 +0600

    dy

---
+++ practise javascript/onclick.js
@@ -0,0 +1,16 @@
+
+
+
+var myVar = document.querySelector("#pra");
+function myPicture1() {
+ myVar.src = "images/mamun.jpeg"
+ 
+
+}
+
+function myPicture2() {
+  myVar.src = "images/karim.jpeg"
+ 
+ }
+ 
+
